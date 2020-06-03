910c55d9ece23e133c29fc8f6bc27f51ca6e2dcb
Albert Sun <albert.sun310@duke.edu>
2020-06-03 11:43:19 -0700

    change metadatascraper to add variables

--- metadatascraper.py
+++ metadatascraper.py
@@ -25,18 +25,31 @@ for url in other_link:
 df = pd.read_csv('addresses-short.csv')
 print(df)
 
-print(df['Websites'].to_string(index=False))
+#print(df['Websites'].to_string(index=False))
 
-authorNames = []
+dates = []
+titles = []
+authors = []
 for url in df['Websites']:
     print(url)
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')  # parse_only=SoupStrainer('tr'))
-    deeperSouper = soup.find('tr', valign='top').getText()
-    authorNames.append(str(deeperSouper))
-    print(deeperSouper)
-    print(authorNames)
+    datepage = requests.get(url)
+    soup = BeautifulSoup(datepage.content, 'html.parser')
+    titleSoup = soup.find('i').getText()
+    authorSoup = soup.find('i').findNext('br').next_element
+    dateSoup = soup.find('i').findNext('br').findNext('br').next_element
+    if len(dateSoup) == 1:
+        dateSoup = authorSoup
+        authorSoup = 'No Author'
+    titles.append(titleSoup)
+    dates.append(dateSoup)
+    authors.append(authorSoup)
 
-df['author'] = authorNames
+df['author'] = authors
+df['title'] = titles
+df['date'] = dates
 
 print(df)
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(df)
+

1e8d91d687f2ede73af8e14ee7a059f4985a0688
Albert Sun <albert.sun310@duke.edu>
2020-06-25 09:43:52 -0700

    proscrutes

--- testword2vec.py
+++ testword2vec.py
@@ -7,15 +7,17 @@ from gensim import utils
 import multiprocessing
 import time
 
-train = pd.read_csv("CSVs/1550-1559.csv", encoding="ISO-8859-1")
-# train = pd.read_csv("CSVs/1470-1479dirty.csv")
+train = pd.read_csv("../CSVs/1580-1589.csv", encoding="ISO-8859-1")
+print("read the document!")
+
 train['booktext'] = train['booktext'].str.split()
+print("split the document into words!")
 
 # time before
 before = time.time()
 
 # create vector
-model = gensim.models.word2vec.Word2Vec(sentences=train["booktext"], workers=4, min_count=0)
+model = gensim.models.word2vec.Word2Vec(sentences=train["booktext"], workers=4, min_count=1, size=300)
 
 # calculate time elapsed
 elapsed = time.time() - before
@@ -25,6 +27,7 @@ for i, word in enumerate(model.wv.vocab):
     j+=1
 print("words count:", j)
 
-print("time elapsed: ", elapsed)
+print("time elapsed for word2vec: ", elapsed)
 
-model.wv.save_word2vec_format("output/word2vec.txt")
+# model.wv.save_word2vec_format("../CSVs/1590w2v.txt")
+model.save("../CSVs/1580w2v.model")

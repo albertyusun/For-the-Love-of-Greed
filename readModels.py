1a63affcb3e18983a9d270bf96993c7a2800a3bf
Donald Pepka <donald.pepka@duke.edu>
2020-07-03 15:06:37 -0400

    added analogy_over_time

--- readModels.py
+++ readModels.py
@@ -133,6 +133,16 @@ def get_vector(date_bucket, word):
     return vectors[word]
 
 
+# given analogy a1 : a2 :: b1 : __, it fills in the blank.
+def analogy_over_time(a1, a2, b1):
+    for date in date_buckets:
+        try:
+            model = gensim.models.Word2Vec.load("C:/Users/djpep/Box Sync/For the Love of Greed Data Storage/models_blank_suffix/" + date)
+            print(date, "", a1, "is to", a2, "as", b1, "is to", model.wv.most_similar_cosmul(positive=[a1, a2], negative=[b1])[0])
+        except KeyError:
+            print("can't find 1-3 vectors in", date)
+
+
 if __name__ == "__main__":
     # code to find cosine similarity:
     print(distance_over_time('consume', 'luxury'))

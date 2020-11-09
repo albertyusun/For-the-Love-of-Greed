943c720d62e7d4e214ae6076a9004ddf5c16952e
Albert Sun <33230608+albertyusun@users.noreply.github.com>
2020-11-09 11:58:51 -0500

    clean up scraper

--- 1_scraping/metadatascraperfast.py
+++ 1_scraping/metadatascraperfast.py
@@ -6,8 +6,15 @@ import requests
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 
-def extract_row(url):
-    datepage = requests.get(url)
+
+def extract_metadata(summary_url):
+    """
+    Extract book metadata and link to book text from the summary page.
+
+    :param url: link to the summary url
+    :return: List of book url, title, author, publisher, date
+    """
+    datepage = requests.get(summary_url)
     soup = BeautifulSoup(datepage.content, 'html.parser')
     titleSoup = soup.find('i').getText()
     authorSoup = soup.find('i').findNext('br').next_element
@@ -15,14 +22,19 @@ def extract_row(url):
     if len(pubSoup) == 1:
         pubSoup = authorSoup
         authorSoup = 'No Author'
-    row = [url, titleSoup, authorSoup, pubSoup, pubSoup[-5:-1]]
-    print("Just got the metadata from: ", url)
-    time.sleep(1.5) #modify as necessary
-    #.5 gives me 426 results until it stops, 1 gives me 486 results until it stops
+    row = [summary_url, titleSoup, authorSoup, pubSoup, pubSoup[-5:-1]]
+    print("Just got the metadata from: ", summary_url)
+    time.sleep(1.5)  # Necessary for their servers to process our parallel requests.
     return row
 
 
-def extract_book(book_link):
+def extract_full_text(book_link):
+    """
+    Extract book text from the book link.
+
+    :param book_link: link to the book contents
+    :return:
+    """
     page = requests.get(book_link)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find(id='doccontent')
@@ -30,70 +42,45 @@ def extract_book(book_link):
     if results is not None:
         r = results.text
         text = r.replace('\n', ' ')
-    time.sleep(1.5) # modify as necessary
+    time.sleep(1.5)  # Necessary for their servers to process our parallel requests.
     print("just copied a book text from ", book_link)
     return text
 
 
 if __name__ == '__main__':
-    # 1. Uncomment out the section you're scraping:
 
-    df = pd.read_csv('CSVs/addresses-short.csv')
-    # df = pd.read_csv('CSVs/addresses_TCP_1_1.csv')
-    # df = pd.read_csv('CSVs/addresses_TCP_1_2_first500.csv')
-    # df = pd.read_csv('CSVs/addresses_TCP_1_3.csv')
-    # df = pd.read_csv('CSVs/addresses_TCP_1_4.csv')
-
-    listInList = [["website", "title", "author", "publishinfo", "dates"]]
-
-    print("Let's extract the initial metadata:")
-
-    # just metadata
     start = time.time()
+
+    df = pd.read_csv('CSVs/addresses.csv')
+
+    print("Let's extract the metadata (url, title, author, publishing info, dates):")
+
+    # Part 1: Metadata Scraping
+
+    rows = [["summary_url", "title", "author", "pub", "dates"]]
     with ThreadPoolExecutor(max_workers=3) as executor:
-        future_titles = {executor.submit(extract_row, url): url for url in df['Websites']}
+        future_titles = {executor.submit(extract_metadata, url): url for url in df['Websites']}
         for future in concurrent.futures.as_completed(future_titles):
-            listInList.append(future.result())
+            rows.append(future.result())
 
-    mid = time.time()
-    print("time for metadata: ", mid - start)  # time it takes to run the metadata scraping
+    new_df = pd.DataFrame(rows[1:], columns=rows[0])
 
-    new_df = pd.DataFrame(listInList[1:],columns=listInList[0])
+    print("Metadata collection completed. Now, let's collect the full text of the books:")
 
-    print("metadata besides book text completed, now onto book text")
+    # Part 2: Full Text Scraping
 
-    # get book links
-    book_links = []
-    for url in df['Websites']:
-        temp = url[:-8]
-        temp = temp + 'rgn=main;view=fulltext'  # clicks on the link that holds the full text
-        book_links.append(temp)
+    full_text_links = [url[:-8] + 'rgn=main;view=fulltext' for url in df['Websites']]  # full text link
 
-    print("book links finished, now onto book text")
-
-    book_text = []
-    # now book text
+    full_text = []
     with ThreadPoolExecutor(max_workers=4) as executor:
-        future_titles = {executor.submit(extract_book, url): url for url in book_links}
+        future_titles = {executor.submit(extract_full_text, url): url for url in full_text_links}
         for future in concurrent.futures.as_completed(future_titles):
-            book_text.append(future.result())
-    print("length: ", len(book_text))
-    #print(book_text)
+            full_text.append(future.result())
 
+    print("Length: ", len(full_text))
 
-    new_df["booktext"] = book_text
+    new_df["fulltext"] = full_text  # add to df
 
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        print(new_df)
+    new_df.to_csv("CSVs/output.csv")
 
-    finish = time.time()
-    print("time for total: ", finish - start)  # time it takes to run the metadata scraping
-
-    # 2. Uncomment out the section you're scraping:
-
-    new_df.to_csv("CSVs/metadata-short.csv")
-    # new_df.to_csv("CSVs/metadata_TCP_1_1.csv")
-    # new_df.to_csv("CSVs/metadata_TCP_1_2.csv")
-    # new_df.to_csv("CSVs/metadata_TCP_1_2_first500.csv")
-    # new_df.to_csv("CSVs/metadata_TCP_1_3.csv")
-    # new_df.to_csv("CSVs/metadata_TCP_1_4.csv")
+    print("Total time elapsed: ", time.time() - start)
